1aa8634069ab9312582ad4472805a14e9642211a
Vasil Tarashev <145684223+waskoooo@users.noreply.github.com>
2023-11-08 09:35:48 +0200

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,37 @@
+while True:
+    balance = 10000
+    print("     ATM    ")
+    print("""
+    1)    Balance
+    2)    Withdraw
+    3)    Deposit
+    4)    Quit  
+    """)
+    try:
+        Option=int(input("Enter Option:"))
+    except Exception as e:
+        print("Error:", e)
+        print("Enter 1,2,3, or 4 only")
+        continue
+    if Option==1:
+        print("Balance  £ ", balance)
+    if Option==2:
+        print("Balance  £", balance)
+        Withdraw=float(input("Enter Withdraw amount £"))
+        if Withdraw>0:
+            forewardbalance=(balance-Withdraw)
+            print("Foreward Balance £ ", forewardbalance)
+        elif Withdraw>balance:
+            print("No funs in account")
+        else:
+            print("None withdraw made")
+    if Option==3:
+        print("Balance  £",balance)
+        Deposit=float(input("Enter deposit amount  £ "))
+        if Deposit>0:
+            forewardbalance=(balance+Deposit)
+            print("Forewardbalance  £",forewardbalance)
+        else:
+            print("None deposit made ")
+    if Option==4:
+        exit()
